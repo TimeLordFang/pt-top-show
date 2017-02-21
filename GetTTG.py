849d523f836fadde821061f055fb686410bcb028
fang <fang0229@gmail.com>
2017-02-21 15:24:03 +0800

    del headers

--- GetTTG.py
+++ GetTTG.py
@@ -6,35 +6,23 @@ import re
 import requests
 import time
 import sqlite3
+import config
 
-headers = {
-    'Accept-Encoding':'gzip, deflate, sdch, br',
-    'Accept-Language':'zh-CN,zh;q=0.8',
-    'Upgrade-Insecure-Requests':'1',
-    'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.100 Safari/537.36',
-    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Cache-Control':'max-age=0',
-    'Referer':'https://totheglory.im/',
-    'Connection':'keep-alive'
-    }
-t_url = 'https://totheglory.im/browse.php?c=M'
-s_url = ''
-f = open('ttg.cookie','r')
-headers['Cookie'] = f.read().strip()
-f.close()
-
-s = requests.Session()
-s.headers.update(headers)
-
-conn = sqlite3.connect('test.db')
-conn.text_factory = str
+url = config.ttg_url
+headers = config.ttg_headers
+passkey = config.ttg_passkey
+t_url = url + "browse.php?c=M"
 
 def showtop():
-    site = 'TTG'
+    conn = sqlite3.connect('pttop.db')
+    conn.text_factory = str
+    cur = conn.cursor()
+    s = requests.Session()
+    s.headers.update(headers)
     r = s.get(t_url)
+    site = 'TTG'
     soup =  BeautifulSoup(r.content, "lxml")
     fulltable =  soup.find("table",{"id" : "torrent_table"})
-    cur = conn.cursor()
     for row in fulltable.find_all("tr",class_=["hover_hr  sticky","hover_hr"],recursive=False):
         site_id = row["id"]
         up_time = row.find_all("td",{"align":"center"})[1].text
@@ -47,11 +35,11 @@ def showtop():
         except:
             name = ''
 
-        t_link = "https://totheglory.im/t/" + site_id + "/"
-        d_link = "https://totheglory.im/dl/"+ site_id + "/passkey"
+        t_link = url + site_id + "/"
+        d_link = url + site_id + passkey
         cur.execute("INSERT INTO PTTOP (SITE,TITLE,NAME,SITE_ID,SIZE,T_LINK,D_LINK,UPLTIME ) VALUES (?,?,?,?,?,?,?,?) " ,  (site,title,name,site_id,size,t_link,d_link,upltime))
         conn.commit()
+    conn.close()
 
 if __name__ == '__main__':
     showtop()
-    conn.close()
